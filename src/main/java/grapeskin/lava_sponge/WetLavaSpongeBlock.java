6304220a38f4a22744e25bda834223c3c99f5451
SAZrX <107967223+GrapeSkin2191@users.noreply.github.com>
2024-05-04 02:38:40 +0800

    Revert "update 1.16"

--- src/main/java/grapeskin/lava_sponge/WetLavaSpongeBlock.java
+++ src/main/java/grapeskin/lava_sponge/WetLavaSpongeBlock.java
@@ -5,7 +5,6 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.fluid.Fluids;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -15,7 +14,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
@@ -46,43 +44,52 @@ public class WetLavaSpongeBlock extends Block {
     }
 
     private boolean absorbWater(World world, BlockPos pos) {
-        LinkedList<Pair<BlockPos, Integer>> queue = Lists.newLinkedList();
-        queue.add(new Pair<BlockPos, Integer>(pos, 0));
+        Queue<Pair<BlockPos, Integer>> queue = Lists.newLinkedList();
+        queue.add(new Pair(pos, 0));
         int i = 0;
-        while (!queue.isEmpty()) {
-            Pair pair = (Pair)queue.poll();
+
+        while(!queue.isEmpty()) {
+            Pair<BlockPos, Integer> pair = (Pair)queue.poll();
             BlockPos blockPos = (BlockPos)pair.getLeft();
             int j = (Integer)pair.getRight();
-            for (Direction direction : Direction.values()) {
+            Direction[] var8 = Direction.values();
+            int var9 = var8.length;
+
+            for(int var10 = 0; var10 < var9; ++var10) {
+                Direction direction = var8[var10];
                 BlockPos blockPos2 = blockPos.offset(direction);
                 BlockState blockState = world.getBlockState(blockPos2);
                 FluidState fluidState = world.getFluidState(blockPos2);
                 Material material = blockState.getMaterial();
-                if (!fluidState.isIn(FluidTags.WATER)) continue;
-                if (blockState.getBlock() instanceof FluidDrainable && ((FluidDrainable)((Object)blockState.getBlock())).tryDrainFluid(world, blockPos2, blockState) != Fluids.EMPTY) {
-                    ++i;
-                    if (j >= 6) continue;
-                    queue.add(new Pair<BlockPos, Integer>(blockPos2, j + 1));
-                    continue;
+                if (fluidState.isIn(FluidTags.WATER)) {
+                    if (blockState.getBlock() instanceof FluidDrainable && !((FluidDrainable)blockState.getBlock()).tryDrainFluid(world, blockPos2, blockState).isEmpty()) {
+                        ++i;
+                        if (j < 6) {
+                            queue.add(new Pair(blockPos2, j + 1));
+                        }
+                    } else if (blockState.getBlock() instanceof FluidBlock) {
+                        world.setBlockState(blockPos2, Blocks.AIR.getDefaultState(), 3);
+                        ++i;
+                        if (j < 6) {
+                            queue.add(new Pair(blockPos2, j + 1));
+                        }
+                    } else if (material == Material.UNDERWATER_PLANT || material == Material.REPLACEABLE_UNDERWATER_PLANT) {
+                        BlockEntity blockEntity = blockState.hasBlockEntity() ? world.getBlockEntity(blockPos2) : null;
+                        dropStacks(blockState, world, blockPos2, blockEntity);
+                        world.setBlockState(blockPos2, Blocks.AIR.getDefaultState(), 3);
+                        ++i;
+                        if (j < 6) {
+                            queue.add(new Pair(blockPos2, j + 1));
+                        }
+                    }
                 }
-                if (blockState.getBlock() instanceof FluidBlock) {
-                    world.setBlockState(blockPos2, Blocks.AIR.getDefaultState(), 3);
-                    ++i;
-                    if (j >= 6) continue;
-                    queue.add(new Pair<BlockPos, Integer>(blockPos2, j + 1));
-                    continue;
-                }
-                if (material != Material.UNDERWATER_PLANT && material != Material.REPLACEABLE_UNDERWATER_PLANT) continue;
-                BlockEntity blockEntity = blockState.getBlock().hasBlockEntity() ? world.getBlockEntity(blockPos2) : null;
-                SpongeBlock.dropStacks(blockState, world, blockPos2, blockEntity);
-                world.setBlockState(blockPos2, Blocks.AIR.getDefaultState(), 3);
-                ++i;
-                if (j >= 6) continue;
-                queue.add(new Pair<BlockPos, Integer>(blockPos2, j + 1));
             }
-            if (i <= 64) continue;
-            break;
+
+            if (i > 64) {
+                break;
+            }
         }
+
         return i > 0;
     }
 
